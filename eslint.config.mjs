a6a4b32dbbd93a1b5e5e34437ac33e7eb2ada3c0
Muhawow <muhammad.guarddin@gmail.com>
2025-12-07 15:41:30 +0800

    first commit for deploy

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = defineConfig([
+  // ...compat.extends("next/core-web-vitals", "next/typescript"),
+   ...nextVitals,
+  {
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+])
+  
+
+
+export default eslintConfig;
